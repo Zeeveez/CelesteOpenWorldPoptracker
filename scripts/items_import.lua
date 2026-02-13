4f133701c72672cf037750e53775af7cde697e4d
Zeeveez <9338660+Zeeveez@users.noreply.github.com>
2026-02-13 18:31:32 +0000

    tracker pane completion

--- scripts/items_import.lua
+++ scripts/items_import.lua
@@ -1,4 +1,5 @@
 Tracker:AddItems("items/checkpoints.json")
+Tracker:AddItems("items/icons.json")
 Tracker:AddItems("items/items.json")
 Tracker:AddItems("items/keys_gems.json")
 Tracker:AddItems("items/settings.json")
