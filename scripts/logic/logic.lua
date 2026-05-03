b847c5bc0917bd854d09b762d3a9335e3a8657f0
Zeeveez <9338660+Zeeveez@users.noreply.github.com>
2026-05-03 04:19:42 +0100

    Added toggle for inclusion of custom logic

--- scripts/logic/logic.lua
+++ scripts/logic/logic.lua
@@ -132,7 +132,7 @@ function CanAccess(location_name, test_item)
     if CanAccessLocation(location_name, {}, false, in_logic_item_cache, test_item) then
         return true -- In logic
     end
-    if Tracker:FindObjectForCode("show_out_of_logic").Active and CanAccessLocation(location_name, {}, true, out_of_logic_item_cache, test_item) then
+    if Tracker:FindObjectForCode("show_out_of_logic").Active and CanAccessLocation(location_name, {}, Tracker:FindObjectForCode("show_custom_logic").Active, out_of_logic_item_cache, test_item) then
         return 5 -- Sequence break - Custom Logic
     end
     return false
