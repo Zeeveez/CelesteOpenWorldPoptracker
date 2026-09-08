fed5f9385b7046fcc196abf41efe129badc031cd
Zeeveez <9338660+Zeeveez@users.noreply.github.com>
2026-09-08 17:55:22 +0100

    Add warning to status bar if loading incorrect slot for game

--- lib/status_bar/init.lua
+++ lib/status_bar/init.lua
@@ -1,10 +1,15 @@
 Tracker:AddItems("lib/status_bar/items.json")
 
 local function PopulateStatusBar(slot_data)
-    local apworld_version = slot_data["apworld_version"]
-    local major = apworld_version // 10000
-    local minor = apworld_version // 100 % 100
-    local build = apworld_version % 100
+    local major = '?'
+    local minor = '?'
+    local build = '?'
+    if slot_data["apworld_version"] then
+        local apworld_version = slot_data["apworld_version"]
+        major = apworld_version // 10000
+        minor = apworld_version // 100 % 100
+        build = apworld_version % 100
+    end
     local game = Archipelago:GetPlayerGame(Archipelago.PlayerNumber)
     local game_version = "Game: "..game.." ("..major.."."..minor.."."..build..")"
 
@@ -19,5 +24,12 @@ local function PopulateStatusBar(slot_data)
 
     local status_bar = Tracker:FindObjectForCode("status_bar")
     status_bar.BadgeText = status
+    
+    if game ~= "Celeste (Open World)" then
+        status_bar.BadgeTextColor = '#ff0000'
+        status_bar.BadgeText = 'Incorrect slot for game! '..status
+    else
+        status_bar.BadgeTextColor = '#ffffff'
+    end
 end
 Archipelago:AddClearHandler("PopulateStatusBar", PopulateStatusBar)
